27c7cace3a53079f4accc43ae5065a103257cd5b
smartfox1203 <smartfox1203@gmail.com>
2022-06-11 16:17:07 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }]
+};
