62a299efbf9044dda0c2ef06cb0fa2953419013f
smartfox1203 <smartfox1203@gmail.com>
2022-06-17 11:37:06 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,8 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "UA-XXXXXXXX-X"
-    }
-  }]
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ]
 };
